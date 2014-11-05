c6a99f197c57f2ffff39dcc48955c0b8b2283d3e
Javier De la Hoz <javierdlahoz@gmail.com>
2014-11-04 21:43:07 -0500

    no solarium repo

---
+++ Solarium/vendor/solarium2/solarium/examples/config.dist.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+    'endpoint' => array(
+        'localhost' => array(
+            'host' => '54.191.160.100',
+            'port' => 8983,
+            'path' => '/solr/',
+        )
+    )
+);
